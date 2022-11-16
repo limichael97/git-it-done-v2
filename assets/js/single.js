1a643b5b9d0023c30c177c6ee2549bd4b3dbf0cb
Michael <michael@thinkgrow.com>
2022-11-16 11:28:06 -0800

    error handling

--- assets/js/single.js
+++ assets/js/single.js
@@ -7,9 +7,12 @@ var getRepoName = function() {
     var queryString = document.location.search;
     var repoName = queryString.split("=")[1];
 
-    getRepoIssues(repoName)
-
-    repoNameEl.textContent = repoName
+    if (repoName) {
+        getRepoIssues(repoName)
+        repoNameEl.textContent = repoName
+    } else {
+        document.location.replace("./index.html")
+    }
 
 }
 
@@ -29,7 +32,7 @@ var getRepoIssues = function(repo) {
         }
 
         else {
-            alert("There was a problem with your request!")
+            document.location.replace("./index.html");
         }
     })
 
